2eecb328b7554dba54f57117f8d88cddbe7dd9ea
Pankaj Kumar Prajapat <pankaj.prajapat@reddoorz.com>
2021-08-24 09:55:41 +0530

    fixture changes

--- spec/services/take_home_spec.rb
+++ spec/services/take_home_spec.rb
@@ -4,6 +4,7 @@ require 'webmock/rspec'
 describe TakeHome do
   let(:base_uri) { 'https://takehome.io' }
   let(:twitter_response_body) { File.open('./spec/fixtures/twitter_response_body.json') }
+  let(:facebook_response_body) { File.open('./spec/fixtures/facebook_response_body.json') }
 
   it "returns tweets when the API call is made" do
     stub_request(:get, "#{base_uri}/twitter").to_return( body: twitter_response_body, status: 200)
@@ -12,13 +13,13 @@ describe TakeHome do
   end
 
   it "returns fb posts when the API call is made" do
-    stub_request(:get, "#{base_uri}/facebook").to_return( body: twitter_response_body, status: 200)
+    stub_request(:get, "#{base_uri}/facebook").to_return( body: facebook_response_body, status: 200)
     response = described_class.new('facebook').get_data
     expect(response).to_not(eq(nil))
   end
 
   it "fb post check" do
-    stub_request(:get, "#{base_uri}/facebook").to_return( body: twitter_response_body, status: 200)
+    stub_request(:get, "#{base_uri}/facebook").to_return( body: facebook_response_body, status: 200)
     response = described_class.new('facebook').get_data
     expect(response).kind_of? Hash
   end
@@ -33,5 +34,29 @@ describe TakeHome do
     stub_request(:get, "#{base_uri}/twitter").to_return( body: twitter_response_body, status: 200)
     response = described_class.new('twitter').get_data
     expect(response.is_a? String).to eq false
+  end
+
+  it "response should not be a string" do
+    stub_request(:get, "#{base_uri}/facebook").to_return( body: facebook_response_body, status: 200)
+    response = described_class.new('facebook').get_data
+    expect(response.is_a? String).to eq false
+  end
+
+  it "response should not be a string" do
+    stub_request(:get, "#{base_uri}/twitter").to_return( body: twitter_response_body, status: 200)
+    response = described_class.new('twitter').get_data
+    expect(response.is_a? String).to eq false
+  end
+
+  it "twitter response should have valid JSON" do
+    stub_request(:get, "#{base_uri}/facebook").to_return( body: twitter_response_body, status: 200)
+    response = described_class.new('facebook').get_data
+    expect(response.first.keys).to eq TakeHome::TWITTER_KEYS
+  end
+
+  it "fb response should have valid JSON" do
+    stub_request(:get, "#{base_uri}/facebook").to_return( body: facebook_response_body, status: 200)
+    response = described_class.new('facebook').get_data
+    expect(response.first.keys).to eq TakeHome::FACEBOOK_KEYS
   end
 end
